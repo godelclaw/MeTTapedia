6adc93d37c72bc757554eb973adbdbc7f55ac999
Oruzi <oruzi@godelclaw>
2026-06-25 01:06:49 +0200

    Add CAP5 witness generator interface

--- lean/mettapedia/Mettapedia/GraphTheory/FourColor.lean
+++ lean/mettapedia/Mettapedia/GraphTheory/FourColor.lean
@@ -2,5 +2,6 @@ import Mettapedia.GraphTheory.FourColor.CAP5BoundaryWord
 import Mettapedia.GraphTheory.FourColor.CAP5AlgebraBridge
 import Mettapedia.GraphTheory.FourColor.CAP5GraphBoundary
 import Mettapedia.GraphTheory.FourColor.CAP5Reducibility
+import Mettapedia.GraphTheory.FourColor.CAP5WitnessGenerator
 import Mettapedia.GraphTheory.FourColor.CDLFlow
 import Mettapedia.GraphTheory.FourColor.Goal
